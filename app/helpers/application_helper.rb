81c7369331a557073be2ef26812c8d6fa6e35508
ZucchiniZe <me@alexb.ninja>
2015-01-13 17:27:45 -0800

    fix nav_link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
   def nav_link(link_text, link_path)
-    class_name = current_page?(link_path) ? 'active' : ''
+    if link_path =~ /#{link_text}/i
+      class_name = 'active'
+    else
+      class_name = ''
+    end
 
     content_tag(:li, :class => class_name) do
       link_to link_text, link_path
