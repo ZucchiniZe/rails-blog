073ffaf419e3afa67c1f77c7a6832cb1e80b9614
ZucchiniZe <me@alexb.ninja>
2015-01-15 14:34:49 -0800

    add reading time

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ActiveRecord::Base
   has_many :comments
 
   validates_presence_of :content, :title
+
+  def word_count
+    self.content.split.size
+  end
+
+  def reading_time
+     (word_count / 180.0).ceil
+  end
 end
